398d41bcdf83103fee378fb46248eef1f4e34806
Moataz <motazosama1111@gmail.com>
2022-09-11 03:41:06 +0200

    improve 1

--- js/js.js
+++ js/js.js
@@ -1,26 +1,21 @@
 let btf = document.getElementById("btx");
+let con = document.getElementById("cont");
 
+onscroll = function () {
+  if (scrollY >= 100) {
+    btf.style.display = "block";
+  } else {
+    btf.style.display = "none";
+  }
+};
 
+btf.onclick = function () {
+  scroll({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+};
 
-  onscroll = function () {
-    if (scrollY >= 100) {
-      btf.style.display = "block";
-    } else {
-        btf.style.display ="none"
-    }
-  };
-  
-
-
-
-  btf.onclick = function () {
-    scroll({
-        top:0,
-        left:0,
-        behavior:"smooth"
-    });
-  };
-
-  console.log(screen.width)
-
+console.log(screen.width);
 
